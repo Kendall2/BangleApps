a97d1f5dd7215f8649035b5f24c1d1fe6da61fa4
Kendall Price <86101946+Kendall2@users.noreply.github.com>
2023-12-15 01:43:18 +0000

    Create app.js

---
+++ apps/popnclock/app.js
@@ -0,0 +1,178 @@
+function getPopn() {
+  return require("heatshrink").decompress(atob("2Gw4f/7//A4NSglAo2Tg9C/OfgPVw+JkmSpICpCqYEDkESAocAAogCBpEAyEBkmACIkApEEwEJFAwRElJBSEYJBCgQKCoEEAoWQgAICKAYvDDQOQHwJBBKAJEEJQRZBIKdAgEQhMgwRBEAQMAGoQFBFIQvBRIMSBYQCBgEBII2CJQOAgBBcYoQsBiUAGoTFCkEAI4MCIIiDIIP5BaEwQ7BBQsCgBNBhAOCkEQJQOALIJBHJoJBDkECEAJZBIKZ9EBQdAgkCfwOQIIVAIIoRBHwIOCIIMkwAjDIIYFBIKeJBRDCBO4LFBJoLFFAoIyBBgKDCJoI7EAQo+QAQtECqgCNkoEByIYTqRBsrdt23bAR5B/IN8SC6RB/IP5B/IP5B/IP5B/IKH///8IMlP//xIIMEyUJgmCpACBCIYLBQeZBRqaBBINkAyBBBwFAIIMBgBBCBYLFzgSDChACBgBKBIgMSoICBYvCGCBYMQgIFCYt5BHpEkIIdBIIrFtIIwRBYoKDFYu5BJQd4XRQYRBrhEggUAABUBgEQoKDCYtZBVYv7FtC6SD/IIgHCIMgEByNbtu27YCPIP5BEYtRBTqQcBINUAACSDtIP5B/IP5B/IP5B/IP5B/IMwEBghBkoEAiKD/QatQAQLF/Yv7F/YoqD/Qf6D/QYpB/IP7FDIYP8IMlP//xIKzF/Yv6D/IIqFB/hBkp//+KDYgSD7qaBBAgMSYvkkAYMEwRB/IP4DBkhB/gMEyhB9wEIYwJB9iEAkBB+gUBIP7FBIP8ShMAYv8AQf+AgJB/iBB/IIMIIP8CpEgihB9wEJgDF/hBB/gEBkBB+wEIIP8QgBB/Yv5BBiUIgDF/IP5BBIAOEIPuQgEkIP0QgKD/gFBIP+AIP7FBIP8kgRB/Yv5BCyBB/Yv5BDhJB/gUBIP4DBIP8QIP5B/IP5BEyBB/QYUkIP0AgJB/wBB/IP5BCyAEBgVW7dt2wCf2sggEJLSdII4hBkEwMBTqaaBABBMFgJqBAAbcBAwigCAAUCEpQAaiFIHYlBkA7DhEkwQMDkGQKwmCoJBkQc8JYxoaKQc9btu27YCK5CDwgQ+LAQXZQeBB/ILjFFiRBBXIjFEaIOQpLFT4ENQbWAhMkgSDIhEEySGBQZ0WGoUbtuAjcB2AIBwCDSgkAu3YwBxDQYlAndspARBQZkEO4cDtk27FsmEAhk2JQKDQgkTEIMQF4SDFoKuB2JBDQZNJlpBD7A7BIIUbtkAIJuQpB3CkHt2EbsUJR4RBBI4MSh3DsO2kAUBghBBZYaMDfwxBE7aJBYpkEgVJQwJBCwEDIIUCgMkyEIIISDBIIUBkESZYgACixBFgFtwEbgOwBAIFBIJQmBDoMggRBB9gKBIIUQHIPSgKDHJQImCyBBLARfIIJO24dkfYKDBCoJBDtsAhT7BII8FbIMgIIqnBkmSARzsBAAwmB20DtuQoEEiZBBiAUBwdoAwNIfYNBYoOxX4NBlrZBsieBAD5B/IIewIIkAu3YwBBCYoNbIIVAndspBBDwzgBIIsBtu0gEt22ANoOAaAPQgEW7QRBrRBPgGAhMkFgRBB22LIIUIgmSgCJCQZMLoEWwNoi3Qlo+DAQO0BwMCIgTFNgESAQOQIJEBAQNJBgKDKIIyDD0RBBBwSDKFgO2EwOCBASDBAgUQtAaBoIHCQYRcDIIMgKwSDMgqGCQAZBLi3bkBoFIIcN23SgKeIgG27KYDIJu2HwMWRIJBKNwMCWAMQBg8CgMkyA4BLhESpEEBQ1t2kAlrCBYoaDC23boECAQJCJMoMkKYQAGpAhBDRMEwBEBE5IAZQYQAJQYQNKQYIJHhMkyVJARpajABdbtq6BARrFKIP4AlgQ1E2EAsEG7EAmEAhpBD5D3H23AgEG7YzSg2ggEF22ABAUF2hACyRBF7dh2EYAoSDE7InFgdhwEYhkGgFhUR4CC23alBfBtEC7UFgWLBgJBGFAJBBQY5BGjY7B4FhwycBQSCbBPQMoi2KgsUgNFgSJBiyDHYoSDOSwKDBtpBBQa0CIIqDJYoaDNgwSB23YQYPbCQhBP7dF22BYoe0rZBHARhBG4mSpMkAUnYc6BBG4uy7Ns21bluW5dlBB/J2nats2zdN03TtJBE4CwDhuG4bJBjdsm3Atg7CjJBxmz/BIINgm0bBAJB/IPXAIIo+B7dgIOsG7ZBCtuwQYMAAQRB0PQLFGIPDFBgO2gZBQ4gdDAUfYF4gAL7JBF7dt2wCXBhmSD6JBFbQJBotiSMpZBIAE8SIJ1JIOEArZBOkpB/IOULtmQB5hBxgMtQf5BB2RBO5BBugEH/4AN/1AIN8AYpoCBIGAA/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4AwA"));
+}
+
+function getMimiframe1() {
+return require("heatshrink").decompress(atob("mtG4f/j+E7//78SD6sk0mSpMkpQCBCZdJBwIUCyVICZeSCIckymQBYcG4A7JkVJksgBYdk2wTFhIUComAhNAEwYaBwATEgVJtukxAFBBgcMyVLE4sEyXbpMQgEFBQcZLAJQFgdsyXSRANKBQcyHY4IC0mJO4JiENoNgEwgvBpZ3CTwg7HNYMkyyhCTwgyBkmwF41pZAUgSQIWBHYwmCMQNIZYNAFYWAHYySBpMtkhiBMoMpKYMgEAW27dt2HJEwVJiVJAQLgCpCzCtoUBTYImCyIXByUpAYNEyEDd4ImBAQJ0B5MoyVBHYNSGwOJHYPJCIQCBEwOSqMkwSeB0g7CoEAmwmDHYPAGgKvBKAQ7Ca4MNEwfZlkAgKDBgEEyQ7CpMAyVbtgUBluyrDzECgI7BUgL4BO4IuBRILLEAAMJHYRoBycl2ShCpI7BAAkSHYQnBk1bboKhByQ7Goh3CCgNM2Q4By3LHY50BMoeZlmWKYI+BsASEPYLyByg7CIAYpBEwsCOgMBboVMOgQmBtiKGeQMCHYWbOgLsC7aKGkVIUIVJm1Jdge2RQywCJYVtTwWW7A7GdgWkxMpk3Zd4cw7ATFpB0BFIUtyHTBQPSpk24ATEpJ0ByMlEYMNmgmBkmYtuAWQp0CrJcB7VpIgNpk0bHYslVYWSrdl23SPwPAhk24INBoATBpQmByabBrck2mQhO27FtfQYTBa4iJCHwOTkuwgck0jCBwDXF7NkDIMl2VLgEG5QnBpI7ExdsdgQ1CYoMNaIKYBO4dIcwNk2RQByVLpkAjYYBHYMAgJBCJQPJsjsBbQOYgE2FoL+BWYcSrcAmXbpIXBtMwHYdEyAUBiRcB2ALDpIjBsA7DxMkyBQCoYLBgaWCAQPAgO2agVKWgREBAQM0VYdJmEDsitCT4IPBgdsAYLsCCgVhKYMpkSbBEwUG7ADB6Q7E4baBRIOST4RfBMQMB2g7E23bksSgEE0gTC7Y+CtoUDydt22SWAMJT4RrBHwXbBwQmCKwOITYKKCgaJBHwIOBCgICBUgStBlMgEYXAAYMbCIYCBKYQBBqSeDUIYmEHwTFBkqeDAAYmFNAgCBCQp6BCgpiBWAIpBCQp6BCgvJWARlBCYqeBCguyoBNBMoJ6BPIQ7ISoI7DCwK1CTw+2SoOUHYVSpDxDTw1twEBlIkBHwZiJHYMCHYMiqR6ECIw7DUAShCPQNsyXSCIUkCgWwgBfCcAcA7Ml0gjCpJ3ECAOQFQVIhsm7VJd4NJdgI7DLgQqCyEbFgVN0gdBk1JtiqDgRiCkEyagY4CsgmEgEEEwKeBhhAC23ZFYNbc4sJkjaCdgcAgywCCYsAA"));
+}
+
+function getMimiframe2() {
+return require("heatshrink").decompress(atob("mtG4f/j+E7//78SD6sk0mSpMkpQCBCZdJBwIUCyVICZeSCIckymQBottwA7HkVJksgCQk2pNgAwcJCgVEwEJoALDgYxBrAHDgQjBkmIAoIzEgxGB2AHDgmSOgMQgEFHQkMIwI7ECgRiBgFKBQkZHYPAHYtt2mJO4ITEmRHBO4uWpMtO4KeFhuSpazFsmSrRaBTwsbEAJNE2wQB2haBkCSBCwSeBHYkLbIVoMoNAZoQOBtg7FKwIvBMQOkxMpKYLKBWYI7DXIVJtMSpNJiTgCM4KzBlu27dsEweRC4OSlIDBohQBgOS7dtCgIKBpZNBoI7BqQbBxI7CkoRBCgImC6MkwSeB0g7CoA7BrYmCtoKBTwJHBKAQ7CO4MBEwY7B20BQYTyBHYVJgBHBEwe2ZYoUBHYNIUIWW7Z3B5dsZYgABhI7CNAOSI4PJkmyZYOwCYkSHYQnC5LIBUIJBBsATEoh3CCgWW7IEB7dky3ACYh0BMoeTlg4C2XJlp0BAAR7BeQOUEwMmpclAgI+BrYmEgR0BgLdCpJAEkomERQLyBgQ7DCIUsEwyKBkVIUIQ7BEwVZYoITFiSwCB4VNEYLsBpdsCYrsC0mJKAKGBKATdBTwtIOgIpCku2jbvCpk2TwtJEYJlBGoNh2gOBmmTtp3FWAdIgE27Vpm3bBAI7GkokBpGQHAOk6QRBzVJ23BBoNACYNKBYORkI7BhoMBgOkHYK5DCYLXCkSYB2Xapu2gcAk3bkmkLgOAa4dEfwNl2nLsuk7cMg3KE4NJHYmJTYNbpguBtOS7ENaIKYBO4ZiBjMky2ZbQUlwEbLgUkgEBIIUgmVJloCBKYNLSQJcCTAKzCiVAYQNabQeWgFsHYNEyAUBiQLBg3bQwPJHYNJWAPZLgUkyBQCEwOAgdJYoIpB7A7BagVKWgUAgwLBgw5CzdtmA7BVoSfBCQMDtgDBtI5CAQI7ClMiTYImCmwmBgDsCCgWYhuyRIOST4UAJoKQBNwIUDmVbssSgEE0gTC7Y+CEwmSsm26SwBhKfCgOwMoXbCgc26VtkmITYKKCAAcNEAJ3D0mapKtBlMgCYsbBwImCFgM0yQuBqSeDPQfbboICC7TFCkqeDAAUDCIgCB2h9EEwpiBCgq5BWAIpBMQ4UF5KwCMoITFHY+SoBNBMoJ6BPIY7HBII7DCwK1CHZHAgGUHYVSpDxDTwIUFwEBlIkBHwbdCEw3bsECHYMiqR6ECIw7DUAShCPQNsyT7BCIMkCgWwgBfCWwQ7B7Ml0gjCpI7DgAQByAqCpENkzmBfwW2yQ7DLgQqCyEbFgVN0gdBk1Jti/DgRiCkEyagY4CsgmEgEEEwKeBhhAC23ZFYNbc4sJkjaCdgbdDwATFgA="));
+}
+
+function getMimiframe3() {
+return require("heatshrink").decompress(atob("mtG4f/j+E7//78SD6sk0mSpMkpQCBCZdJBwIUCyVICZeSCIckymQHZ8ipMlkAOGhuAAgUJCgVEwEJoATG5NgAgUCEYMkxAFBDwYACgOS4AFCgmSOgMQgEFEw0ZkmwAwYUBMQMApQTGmVJHYp3BxJ3BCY1kHYgOBO4aeGgxcBLAdJlKzDTw0MyVYJorIDkCSBCwZiFhMlk3bpAXBoAZCGoRiFiVLKwPJWwOJlJTBZQRiFkuyKwIjBpMSNARnC5JiE7JMBpYCByRoCohQBYoNbtu27FsI4WWoI7BqSkCHYMDkm27dt2AmCpMswSeB0g7CoCeBpYmB7YmDyXINALLCHYJKBgImCAQImDBYKkBHYVJgBZBCIQCBBQO2PocEHYKkBLIQmDHYPbaYkJHYRoByUt2x3By3ZljnFiQ7CE4SeBAgK2BeQYACoh3CCgWSQwVbIIJNDAAJ0BMoeTlrUC23JlrLDQYKwBkmUEwMmrI4BpdlFIImEgR0BgLdCpmWcwQ+B2wTEgmRkkCHYZnDFINsRQsipChCHYJ8Ey3YRQqwCB4VMCIRlBWwITEdgWkxJQBQwLsCZYI7FpB0BbQaGBHYVsgw7FpIjBMoIOCmzvCzEAmyyFOgVIAwPbtKtBtMmgA7FkokBpGQAwM26QmBFoOAKYOSoAMBpQmByMggEDtL1DyYaBKwQTBa4UiDQMG7QRBwFNknYtukLgOAa4dEJQMN2gaBg3TgOyrYaBpI7ExIFB5MtDQNsUIICB6SYBO4ZiChjpBboUtyVZpo7CgEBkgFBMQMZYQLdCZYOyzZcBTASzBiRiBmQjBkhxBy3JliPBomQCgMSJ4IYBsmSrVsRYQ7BtpcBkitCUgU27Mt0nZcAQpBSQVKWgQABgbCB6RiD2VJsqSCT4ISCgKDBhokCC4NN20JtMiTYImD7YCBjRLCbQVkyxBCT4TCB2ADBmhgDHwK5B2UAgmkCYUbsBQBEwmSpuUzdkgEJT4R0BAQMGEwgCBtM25CbBRQUAEwI+B0gRCzZ9CtqtBlLVBAAPAHwQmDm3btuk7QHBqSeEHwSqCEwXbpOkxMlTwizEEYICDKwgmFgwRFAQLXBCgOJExqwDpMSExuyoBNByUkPQJ5ChomGcYQ7DCwK1DHY4JByg7CqVIeIcbCg2AgMpEgI+DCQMBHY9ggQ7BkVSPQhiH2zRBUAShCPQNsyTmBCIMkCgWwgBfCWwQ7B7Ml0gjCpI7DgAQByAqCpENkzsBfwNJa4I7DLgQqCyEbFgVN0gdBk1JtjADgRiCkEyHAXJHAVkEwkAggmBTwMMIATpDrbnEgEJkjaCgbUDeQWACYsAA=="));
+}
+
+function getMimiframe4() {
+return require("heatshrink").decompress(atob("mtG4f/j+E7//78SD6sk0mSpMkpQCBCZdJBwIUCyVICZeSCIckymQHZ8ipMlkAOGhuwAgUJCgVEwEJoATG5MsAgUCEYMkxAFBwASFgOSrAFCgmSOgMQgEFEw0Zkg7DCgRiBgFKCY0ypNgHYh3BxJ3BCY1kyXAO4eUO4aeGgxcBLAdJlKzDTw0MyVLJorIDkCSBCwZiFhKJBa4IXBoAZCGoRiFiVKpu2MQOkxMpKYLKCMQ2W5MlEYNJiRoCM4TFEgEbDQNJAQOSNAVEKALFBrdt23YtphD4I7BqSkCHYMDkm27dt2HbOYUlwSeB0g7CoCeBpYmB7dsEwaSBiTLCHYJ3BhgmCAQImDSQKkBHYVJgAdBCIQCBD4XYXIY7BUgILBywmDHYICBPocJDYRoByUt2x3By3ZR4i5CHYQnCVQaYBrATFoh3CCgWS7IEBrZBBZYYABOgJlDyct2RxB23Jlr4CAAMBWAMkygmBk1ZHANLsopBEwkCOgMBboVMy1LAgI+B2wTEgmRkkCHYZnDFIKJERQMipChCHYJ8EyywDRQSwCB4VMCIRlBtrXBAAbsC0mJKAKGBdgTLBHYtIOgLaE5I7CtkG7YTEpIjBMoIOBdgQCBzEAmyyFOgVIAwPapKtBtMmgENWYklEgNIyC5BPQImBFoOAja5BoATBpQmByMggEbhqfEtkWKwQTBa4UiDQM2HYMkwFMgHYlu0LgOAa4dEIgNk2wUBgGTtuypdpkgHBHYeJgEGpdky3SpM25Ms03aTAJ3DMQUMJgW0zclyVZpo7COAMk0hiCjJhCtM2pbdBzdpfwKzDiRiBmRzC6YCB7Y7B6VEyAUBiQOBDANsEwMl2QXCpdNfYMkJQJQBUgT7ByXaVoL1CrYDBpQPCAAc26dtkmZkjyBsu0DATeEAAIRBBYMm7dN2EbMQKbBCQsGYoQgCya5B6IHBT4QACjdsmjsDFIPJtoQBgmkCQcB23DEwmSpuU7SDBhKfCOgQ7BEwkkzdJ0mITYKKCgEN2wCBCIlt2wFBVoMpaoJNDgEaEwc27Z9CAINSTwc24BQB6VJTYImBPoWJkqeGgYOBCIQCCKwgSEEwIRFHYSwBFIImN7KwCMoLFFEw+yoBNBMoJ6BPISMBCgwKBHYYWBWoY7HBIOUHYVSpDxDjYUGwEBlIkBHwZ3KsECHYMiqR6EMQ+24EAUAShCPQNsyXSCIUkCgWwgBfCWwQ7B7Ml0gjCpI7DgAQByAqCpENkzmBfwLvByQ7DLgQqCyEbFgVN0gdBk1JtjADgRiCkEyHAXJHAVkEwkAggmBTwMMIATpDrbnEgEJkjaCgbUDeQWACYsA"));
+}
+
+// returns the temperature from weather json, if weather is provided trough 
+//android/ios .. if no temperature is available, it wont be displayed.
+//(returns empty string).
+function getTemperature() {
+  try {
+    var weatherJson = storage.readJSON('weather.json');
+    var weather = weatherJson.weather;
+    return "Temp: " + Math.round(weather.temp - 273.15);
+  } catch (ex) {
+    return "";
+  }
+}
+
+const storage = require('Storage');
+require("Font8x16").add(Graphics);
+
+var IMAGEWIDTH = 176;
+var IMAGEHEIGHT = 176;
+var energySave = false;
+var batteryLvl = E.getBattery() + "%";
+var temperature = getTemperature();
+
+// timeout used to update every minute
+var drawTimeout;
+
+// schedule a draw for the next minute
+function queueDraw() 
+{
+  if (drawTimeout) clearTimeout(drawTimeout);
+  drawTimeout = setTimeout(function () 
+  {
+    drawTimeout = undefined;
+    draw();
+  }, (energySave == true ? 60000 : 1000) - (Date.now() % (energySave == true ? 60000 : 1000)));
+}
+
+var framenum = 0;
+function framecounter(){
+if (framenum <= 6){
+  framenum = framenum + 1;
+  print(framenum);
+}
+  else{
+    framenum = 1;
+  }
+}
+setInterval(framecounter, 1000);
+function draw() {
+  
+  // sometimes, when gadgetbridge sends a message, the locked event is not thrown after the messages was displayed.
+  // this checks the lock state manually and set the energysave.
+  if (!energySave && Bangle.isLocked()) { energySave=true; }
+
+  var date = new Date();
+  var x = 10;
+  var y = 2;
+
+  g.reset();
+
+  //draw main background image
+  g.drawImage(getPopn(), 0, 0);
+  
+  //draw animated mimi
+  if(framenum == 1){
+  g.drawImage(getMimiframe1(), 115, 37);
+  }
+  else if(framenum == 2){
+  g.drawImage(getMimiframe2(), 115, 37);
+  }
+  else if(framenum == 3){
+  g.drawImage(getMimiframe3(), 115, 37);
+  }
+  else if(framenum == 4){
+  g.drawImage(getMimiframe4(), 115, 37);
+  }
+  else if(framenum == 5){
+  g.drawImage(getMimiframe3(), 115, 37);
+  }
+  else if(framenum == 6){
+  g.drawImage(getMimiframe2(), 115, 37);
+  }
+  else if(framenum == 7){
+  g.drawImage(getMimiframe1(), 115, 37);
+  }
+
+  // only update the batterylvl and temperature every 30 seconds or every minute if energysave is on
+  if (date.getSeconds() % 30 == 0) 
+  {
+    batteryLvl = E.getBattery() + "%";
+    temperature = getTemperature();
+  }
+
+  g.setFont("8x16");
+  g.drawString(temperature, 4, g.getHeight() - 40);
+  g.drawString(batteryLvl, g.getWidth() - g.stringWidth(batteryLvl) - 60, 153);
+
+  // work out locale-friendly date/time
+  var h = date.getHours();
+  var m = date.getMinutes();
+  var s = date.getSeconds();
+
+  var timeStr = h.toString().padStart(2, 0) + ':' + m.toString().padStart(2, 0);
+  var timeStr2 = s.toString().padStart(2, 0);
+
+  var dateStr = require("locale").date(new Date(), 1);
+  dateStr = dateStr.replace(new RegExp('/', 'g'), '.');
+
+  var nameOfCurrentDay = require("locale").dow(date, 0).toUpperCase();
+
+  //---Hour and minute---
+  g.setColor("#FFFFFF");
+  g.setFont("8x16", 2);
+  g.drawString(timeStr, x+5, y+6);
+
+  // ---Seconds---
+  x += 2 + g.stringWidth(timeStr);
+  y += 24;
+
+  //only print seconds if screen is active and not locked
+  if (!energySave) {
+    g.setFont("8x16");
+    g.drawString(timeStr2, x+5, y);
+  }
+
+  // ---draw date---
+  y += 45;
+  g.setColor("#FFFFFF");
+  g.setFont("8x16");
+  g.drawString(nameOfCurrentDay, 15, y);
+  y += 15;
+  g.drawString(dateStr, 15, y);
+  queueDraw();
+}
+
+
+// Stop updates when LCD is off, restart when on
+Bangle.on('lock', on => {
+  if (on) {
+    energySave = true;
+  }
+  else {
+    energySave = false;
+  }
+  draw();
+});
+
+g.clear();
+draw();
+
+// Show launcher when middle button pressed
+Bangle.setUI("clock");
+Bangle.loadWidgets();
+try {
+  require("widget_utils").swipeOn(); // hide widgets, make them visible with a swipe
+}
+catch (ex) {
+  require("widget_utils").hide();
+}
